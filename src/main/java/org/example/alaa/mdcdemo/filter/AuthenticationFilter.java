632667141ee550e35782caacefa40cf2347337f8
Alaa Apuelsoud <aabouelsoud@ntgclarity.com>
2025-11-30 15:17:11 +0200

    Filters

---
+++ src/main/java/org/example/alaa/mdcdemo/filter/AuthenticationFilter.java
@@ -0,0 +1,52 @@
+package org.example.alaa.mdcdemo.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.alaa.mdcdemo.context.UserContext;
+import org.example.alaa.mdcdemo.model.UserInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * Simulated Authentication filter
+ */
+@Component
+@Order(value = 2)
+public class AuthenticationFilter extends OncePerRequestFilter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationFilter.class);
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        /**
+         * Simulation for validating user token in the request.
+         * if user authenticated spring security will generate a new authentication object.
+         * extract userInfo form the authentication Object.
+         * add the object to the userContext.
+         */
+        try {
+            //loggedIn user data getting from authentication object
+            UserInfo userInfo = UserInfo.builder()
+                    .userId(1)
+                    .userName("alaaapuelsoad")
+                    .realmId(1)
+                    .role("admin")
+                    .build();
+
+            UserContext.setUserContext(userInfo);
+
+            filterChain.doFilter(request, response);
+
+        }catch (Exception e){
+           throw new RuntimeException("error in request - [ " + MDC.get("X-Correlation-ID")+" ]");
+        }
+
+    }
+}
